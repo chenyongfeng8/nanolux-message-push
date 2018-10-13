4bb81abb408c3651259878ede2bceedd946149bf
dell <dell@DESKTOP-4CA55O9>
2018-10-13 17:11:31 +0800

    cyf 1

--- src/main/java/com/nanolux/socketio/server/WebSocketLauncher.java
+++ src/main/java/com/nanolux/socketio/server/WebSocketLauncher.java
@@ -1,18 +1,12 @@
 package com.nanolux.socketio.server;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import com.corundumstudio.socketio.AckRequest;
-import com.corundumstudio.socketio.BroadcastOperations;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.SocketIONamespace;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
-import com.corundumstudio.socketio.namespace.Namespace;
 import com.nanolux.socketio.server.vo.WebSocketVO;
 
 public class WebSocketLauncher {
