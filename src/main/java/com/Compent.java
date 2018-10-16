a4db2e7b5611d3dd73a3f59b1777592a09212eca
dell <dell@DESKTOP-4CA55O9>
2018-10-16 17:11:29 +0800

    commit

--- src/main/java/com/Compent.java
+++ src/main/java/com/Compent.java
@@ -1,18 +1,18 @@
-package com;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-/**
- * Created by java技术.
- */
-@Component
-public class Compent {
-
-	@Value("${server.port}")
-	private String port;
-
-	public void show() {
-		System.out.println("i am compent:"+port);
-	}
-}
+//package com;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * Created by java技术.
+// */
+//@Component
+//public class Compent {
+//
+//	@Value("${server.port}")
+//	private String port;
+//
+//	public void show() {
+//		System.out.println("i am compent:"+port);
+//	}
+//}
--- src/main/java/com/Config.java
+++ src/main/java/com/Config.java
@@ -1,34 +1,34 @@
-package com;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * Created by java技术.
- */
-@Configuration("name")//表示这是一个配置信息类,可以给这个配置类也起一个名称
-//@ComponentScan("spring4")//类似于xml中的<context:component-scan base-package="spring4"/>
-public class Config {
-
-//    @Autowired//自动注入，如果容器中有多个符合的bean时，需要进一步明确
-//    @Qualifier("compent")//进一步指明注入bean名称为compent的bean
-//    private Compent compent;
+//package com;
 //
-//    @Autowired
-//    @Qualifier("newbean")
-//    private Compent newbean;
-
-    @Bean//类似于xml中的<bean id="newbean" class="spring4.Compent"/>
-    public Compent newbean(){
-        return new Compent();
-    }
-
-    public void print(){
-        System.out.println("i am beanname");
-//        compent.show();
-//        newbean.show();
-    }
-}
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Created by java技术.
+// */
+//@Configuration("name")//表示这是一个配置信息类,可以给这个配置类也起一个名称
+////@ComponentScan("spring4")//类似于xml中的<context:component-scan base-package="spring4"/>
+//public class Config {
+//
+////    @Autowired//自动注入，如果容器中有多个符合的bean时，需要进一步明确
+////    @Qualifier("compent")//进一步指明注入bean名称为compent的bean
+////    private Compent compent;
+////
+////    @Autowired
+////    @Qualifier("newbean")
+////    private Compent newbean;
+//
+//    @Bean//类似于xml中的<bean id="newbean" class="spring4.Compent"/>
+//    public Compent newbean(){
+//        return new Compent();
+//    }
+//
+//    public void print(){
+//        System.out.println("i am beanname");
+////        compent.show();
+////        newbean.show();
+//    }
+//}
